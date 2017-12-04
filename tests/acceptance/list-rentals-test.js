e3ea18b562a3a4b8a0b8be308da2f6a6ac8deb09
Jason Sadler <jason.r.sadler@gmail.com>
2017-12-04 12:44:17 -0500

    Finished up super-rentals

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -1,7 +1,19 @@
 import { test } from 'qunit';
 import moduleForAcceptance from 'super-rentals/tests/helpers/module-for-acceptance';
+import Service from '@ember/service';
 
-moduleForAcceptance('Acceptance | list rentals');
+let StubMapsService = Service.extend({
+  getMapElement() {
+    return document.createElement('div');
+  }
+});
+
+moduleForAcceptance('Acceptance | list-rentals', {
+  beforeEach() {
+    this.application.register('service:stubMaps', StubMapsService);
+    this.application.inject('component:location-map', 'maps', 'service:stubMaps');
+  }
+});
 
 test('should show rentals as the home page', function(assert) {
   visit('/');
@@ -42,3 +54,13 @@ test('should filter the list of rentals by city', function (assert) {
     assert.equal(find('.listing .location:contains("Seattle")').length, 1, 'should contain 1 listing with location Seattle');
   });
 });
+
+test('should show details for a specific rental', function(assert){
+  visit('/rentals');
+  click('a:contains("Grand Old Mansion")');
+  andThen(function() {
+    assert.equal(currentURL(), '/rentals/grand-old-mansion', 'should navigate to show route');
+    assert.equal(find('.show-listing h2').text(), "Grand Old Mansion", 'should list rental title');
+    assert.equal(find('.description').length, 1, 'should list a description of the property');
+  })
+})
